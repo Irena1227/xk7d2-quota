b0f6624b720637487f5346fa41c2077d9b729a6e
Irena <218455917+Irena1227@users.noreply.github.com>
2026-07-19 18:36:18 +0800

    更新额度数据 2026/07/19 18:36

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.DASH_DATA = {
-  "updatedAt": "2026-07-19T18:23:40.531+08:00",
+  "updatedAt": "2026-07-19T18:33:40.078+08:00",
   "sources": {
     "deepseek": {
       "ok": true,
@@ -7,7 +7,7 @@ window.DASH_DATA = {
       "balance": 26.97,
       "currency": "CNY",
       "detail": "余额 ¥26.97",
-      "fetchedAt": "2026-07-19T18:23:38.632+08:00",
+      "fetchedAt": "2026-07-19T18:33:38.638+08:00",
       "error": null
     },
     "claude": {
@@ -16,16 +16,16 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "5小时",
-          "usedPct": 7,
-          "resetAt": "2026-07-19T21:20:00.350+08:00"
+          "usedPct": 17,
+          "resetAt": "2026-07-19T21:20:00.428+08:00"
         },
         {
           "name": "7天",
-          "usedPct": 41,
-          "resetAt": "2026-07-23T20:00:00.350+08:00"
+          "usedPct": 42,
+          "resetAt": "2026-07-23T20:00:00.428+08:00"
         }
       ],
-      "fetchedAt": "2026-07-19T18:23:38.677+08:00",
+      "fetchedAt": "2026-07-19T18:33:38.685+08:00",
       "error": null
     },
     "codex": {
@@ -35,10 +35,10 @@ window.DASH_DATA = {
         {
           "name": "周",
           "usedPct": 55,
-          "resetAt": "2026-07-25T11:27:41.000+08:00"
+          "resetAt": "2026-07-25T11:27:42.000+08:00"
         }
       ],
-      "fetchedAt": "2026-07-19T18:23:38.679+08:00",
+      "fetchedAt": "2026-07-19T18:33:38.686+08:00",
       "error": null
     },
     "kimi": {
@@ -51,7 +51,7 @@ window.DASH_DATA = {
           "resetAt": "2026-07-19T20:02:02.691+08:00"
         }
       ],
-      "fetchedAt": "2026-07-19T18:23:38.687+08:00",
+      "fetchedAt": "2026-07-19T18:33:38.696+08:00",
       "error": null
     }
   }
