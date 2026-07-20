15cbcec32cd804abff8a2e38b64fc1a43ca8ec42
Irena <218455917+Irena1227@users.noreply.github.com>
2026-07-20 23:16:17 +0800

    更新额度数据 2026/07/20 23:16

--- live-endpoint.js
+++ live-endpoint.js
@@ -1,1 +1,1 @@
-window.DASH_LIVE_ENDPOINT = 'https://mai-assure-nodes-basic.trycloudflare.com/data.js';
+window.DASH_LIVE_ENDPOINT = 'https://voices-halloween-delicious-courtesy.trycloudflare.com/data.js';
