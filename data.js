54979ad1287c65322bdd74646b725c6b17cfa4e4
Irena <218455917+Irena1227@users.noreply.github.com>
2026-07-23 11:06:17 +0800

    更新额度数据 2026/07/23 11:06

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.DASH_DATA = {
-  "updatedAt": "2026-07-23T10:54:02.304+08:00",
+  "updatedAt": "2026-07-23T11:06:02.727+08:00",
   "weather": {
     "ok": true,
     "provider": "cyberboss",
@@ -7,12 +7,12 @@ window.DASH_DATA = {
     "iconKey": "rain",
     "tempC": 23,
     "feelsLikeC": 25,
-    "humidity": 96,
-    "windKph": 9,
-    "windDir": "北风",
+    "humidity": 97,
+    "windKph": 8,
+    "windDir": "东北风",
     "place": "北京",
-    "observedAt": "2026-07-23T09:59:05.943+08:00",
-    "fetchedAt": "2026-07-23T10:54:02.304+08:00",
+    "observedAt": "2026-07-23T10:59:12.295+08:00",
+    "fetchedAt": "2026-07-23T11:06:02.727+08:00",
     "error": null
   },
   "sources": {
@@ -22,7 +22,7 @@ window.DASH_DATA = {
       "balance": 26.41,
       "currency": "CNY",
       "detail": "余额 ¥26.41",
-      "fetchedAt": "2026-07-23T10:54:00.301+08:00",
+      "fetchedAt": "2026-07-23T11:06:00.310+08:00",
       "error": null
     },
     "claude": {
@@ -31,16 +31,16 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "5小时",
-          "usedPct": 20,
-          "resetAt": "2026-07-23T15:10:00.167+08:00"
+          "usedPct": 21,
+          "resetAt": "2026-07-23T15:10:00.248+08:00"
         },
         {
           "name": "7天",
           "usedPct": 80,
-          "resetAt": "2026-07-23T20:00:00.167+08:00"
+          "resetAt": "2026-07-23T20:00:00.248+08:00"
         }
       ],
-      "fetchedAt": "2026-07-23T10:54:00.347+08:00",
+      "fetchedAt": "2026-07-23T11:06:00.356+08:00",
       "error": null
     },
     "codex": {
@@ -53,7 +53,7 @@ window.DASH_DATA = {
           "resetAt": "2026-07-29T01:36:11.000+08:00"
         }
       ],
-      "fetchedAt": "2026-07-23T10:54:00.349+08:00",
+      "fetchedAt": "2026-07-23T11:06:00.358+08:00",
       "error": null
     },
     "kimi": {
@@ -77,7 +77,7 @@ window.DASH_DATA = {
           "observedAt": "2026-07-23T08:00:04.233+08:00"
         }
       ],
-      "fetchedAt": "2026-07-23T10:54:00.359+08:00",
+      "fetchedAt": "2026-07-23T11:06:00.368+08:00",
       "error": null
     }
   },
