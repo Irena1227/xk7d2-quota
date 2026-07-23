c751407844b73b69232d71fb0627b70f041e592c
Irena <218455917+Irena1227@users.noreply.github.com>
2026-07-23 22:36:18 +0800

    更新额度数据 2026/07/23 22:36

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.DASH_DATA = {
-  "updatedAt": "2026-07-23T22:24:02.042+08:00",
+  "updatedAt": "2026-07-23T22:36:01.857+08:00",
   "weather": {
     "ok": true,
     "provider": "cyberboss",
@@ -12,7 +12,7 @@ window.DASH_DATA = {
     "windDir": "东南风",
     "place": "北京",
     "observedAt": "2026-07-23T21:00:23.300+08:00",
-    "fetchedAt": "2026-07-23T22:24:02.042+08:00",
+    "fetchedAt": "2026-07-23T22:36:01.857+08:00",
     "error": null
   },
   "sources": {
@@ -22,7 +22,7 @@ window.DASH_DATA = {
       "balance": 25.95,
       "currency": "CNY",
       "detail": "余额 ¥25.95",
-      "fetchedAt": "2026-07-23T22:24:00.349+08:00",
+      "fetchedAt": "2026-07-23T22:36:00.313+08:00",
       "error": null
     },
     "claude": {
@@ -31,16 +31,16 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "5小时",
-          "usedPct": 16,
-          "resetAt": "2026-07-24T01:59:59.600+08:00"
+          "usedPct": 17,
+          "resetAt": "2026-07-24T02:00:00.506+08:00"
         },
         {
           "name": "7天",
           "usedPct": 2,
-          "resetAt": "2026-07-30T19:59:59.600+08:00"
+          "resetAt": "2026-07-30T20:00:00.506+08:00"
         }
       ],
-      "fetchedAt": "2026-07-23T22:24:00.422+08:00",
+      "fetchedAt": "2026-07-23T22:36:00.359+08:00",
       "error": null
     },
     "codex": {
@@ -53,7 +53,7 @@ window.DASH_DATA = {
           "resetAt": "2026-07-29T01:36:11.000+08:00"
         }
       ],
-      "fetchedAt": "2026-07-23T22:24:00.424+08:00",
+      "fetchedAt": "2026-07-23T22:36:00.361+08:00",
       "error": null
     },
     "kimi": {
@@ -77,7 +77,7 @@ window.DASH_DATA = {
           "observedAt": "2026-07-23T08:00:04.233+08:00"
         }
       ],
-      "fetchedAt": "2026-07-23T22:24:00.437+08:00",
+      "fetchedAt": "2026-07-23T22:36:00.372+08:00",
       "error": null
     }
   },
