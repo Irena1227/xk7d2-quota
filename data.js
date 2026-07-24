ce3ea9b4dc7be65c2642227fde9bd0dfe333252a
Irena <218455917+Irena1227@users.noreply.github.com>
2026-07-24 16:06:17 +0800

    更新额度数据 2026/07/24 16:06

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.DASH_DATA = {
-  "updatedAt": "2026-07-24T15:54:01.714+08:00",
+  "updatedAt": "2026-07-24T16:06:03.001+08:00",
   "weather": {
     "ok": true,
     "provider": "cyberboss",
@@ -12,7 +12,7 @@ window.DASH_DATA = {
     "windDir": "北风",
     "place": "北京",
     "observedAt": "2026-07-24T15:02:40.893+08:00",
-    "fetchedAt": "2026-07-24T15:54:01.714+08:00",
+    "fetchedAt": "2026-07-24T16:06:03.001+08:00",
     "error": null
   },
   "sources": {
@@ -22,7 +22,7 @@ window.DASH_DATA = {
       "balance": 25.8,
       "currency": "CNY",
       "detail": "余额 ¥25.80",
-      "fetchedAt": "2026-07-24T15:54:00.313+08:00",
+      "fetchedAt": "2026-07-24T16:06:00.306+08:00",
       "error": null
     },
     "claude": {
@@ -32,15 +32,15 @@ window.DASH_DATA = {
         {
           "name": "5小时",
           "usedPct": 13,
-          "resetAt": "2026-07-24T18:00:00.058+08:00"
+          "resetAt": "2026-07-24T18:00:00.286+08:00"
         },
         {
           "name": "7天",
           "usedPct": 8,
-          "resetAt": "2026-07-30T20:00:00.058+08:00"
+          "resetAt": "2026-07-30T20:00:00.286+08:00"
         }
       ],
-      "fetchedAt": "2026-07-24T15:54:00.358+08:00",
+      "fetchedAt": "2026-07-24T16:06:00.350+08:00",
       "error": null
     },
     "codex": {
@@ -49,11 +49,11 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "周",
-          "usedPct": 42,
+          "usedPct": 43,
           "resetAt": "2026-07-29T01:36:11.000+08:00"
         }
       ],
-      "fetchedAt": "2026-07-24T15:54:00.359+08:00",
+      "fetchedAt": "2026-07-24T16:06:00.352+08:00",
       "error": null
     },
     "kimi": {
@@ -77,7 +77,7 @@ window.DASH_DATA = {
           "observedAt": "2026-07-24T08:00:04.127+08:00"
         }
       ],
-      "fetchedAt": "2026-07-24T15:54:00.369+08:00",
+      "fetchedAt": "2026-07-24T16:06:00.361+08:00",
       "error": null
     }
   },
