52a942a81e4e2eb12f1fbbe2536c4426cd9a60ca
Irena1227 <218455917+Irena1227@users.noreply.github.com>
2026-07-27 21:46:17 +0800

    更新额度数据 2026/07/27 21:46

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 window.DASH_DATA = {
-  "updatedAt": "2026-07-27T21:36:07.025+08:00",
+  "updatedAt": "2026-07-27T21:45:06.255+08:00",
   "weather": {
     "ok": true,
     "provider": "cyberboss",
-    "description": "多云",
-    "iconKey": "cloudy",
-    "tempC": 31,
-    "feelsLikeC": 33,
-    "humidity": 71,
-    "windKph": 15,
+    "description": "雾霾",
+    "iconKey": "fog",
+    "tempC": 26,
+    "feelsLikeC": 30,
+    "humidity": 96,
+    "windKph": 3,
     "windDir": "东南风",
     "place": "北京",
-    "observedAt": "2026-07-27T12:12:17.839+08:00",
-    "fetchedAt": "2026-07-27T21:36:07.025+08:00",
+    "observedAt": "2026-07-27T21:38:48.180+08:00",
+    "fetchedAt": "2026-07-27T21:45:06.256+08:00",
     "error": null
   },
   "sources": {
@@ -22,7 +22,7 @@ window.DASH_DATA = {
       "balance": 25.17,
       "currency": "CNY",
       "detail": "余额 ¥25.17",
-      "fetchedAt": "2026-07-27T21:36:00.323+08:00",
+      "fetchedAt": "2026-07-27T21:45:00.748+08:00",
       "error": null
     },
     "claude": {
@@ -31,16 +31,16 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "5小时",
-          "usedPct": 29,
-          "resetAt": "2026-07-27T22:50:00.910+08:00"
+          "usedPct": 34,
+          "resetAt": "2026-07-27T22:50:00.257+08:00"
         },
         {
           "name": "7天",
-          "usedPct": 40,
-          "resetAt": "2026-07-30T20:00:00.910+08:00"
+          "usedPct": 41,
+          "resetAt": "2026-07-30T20:00:00.257+08:00"
         }
       ],
-      "fetchedAt": "2026-07-27T21:36:00.405+08:00",
+      "fetchedAt": "2026-07-27T21:45:00.802+08:00",
       "error": null
     },
     "codex": {
@@ -49,11 +49,11 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "周",
-          "usedPct": 42,
+          "usedPct": 43,
           "resetAt": "2026-08-02T03:30:23.000+08:00"
         }
       ],
-      "fetchedAt": "2026-07-27T21:36:00.407+08:00",
+      "fetchedAt": "2026-07-27T21:45:00.805+08:00",
       "error": null
     },
     "kimi": {
@@ -62,7 +62,7 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "5小时",
-          "usedPct": 11,
+          "usedPct": 12,
           "resetAt": "2026-07-27T23:02:02.691+08:00"
         },
         {
@@ -77,7 +77,7 @@ window.DASH_DATA = {
           "observedAt": "2026-07-26T08:00:03.880+08:00"
         }
       ],
-      "fetchedAt": "2026-07-27T21:36:01.539+08:00",
+      "fetchedAt": "2026-07-27T21:45:00.832+08:00",
       "error": null
     }
   },
