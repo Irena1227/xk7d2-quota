a6b764687dd64e2d2638007364d3187e88ace8c2
Irena1227 <218455917+Irena1227@users.noreply.github.com>
2026-07-27 18:26:17 +0800

    更新额度数据 2026/07/27 18:26

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.DASH_DATA = {
-  "updatedAt": "2026-07-27T18:15:12.628+08:00",
+  "updatedAt": "2026-07-27T18:24:12.718+08:00",
   "weather": {
     "ok": true,
     "provider": "cyberboss",
@@ -12,7 +12,7 @@ window.DASH_DATA = {
     "windDir": "东南风",
     "place": "北京",
     "observedAt": "2026-07-27T12:12:17.839+08:00",
-    "fetchedAt": "2026-07-27T18:15:12.628+08:00",
+    "fetchedAt": "2026-07-27T18:24:12.719+08:00",
     "error": null
   },
   "sources": {
@@ -22,7 +22,7 @@ window.DASH_DATA = {
       "balance": 25.27,
       "currency": "CNY",
       "detail": "余额 ¥25.27",
-      "fetchedAt": "2026-07-27T18:15:00.308+08:00",
+      "fetchedAt": "2026-07-27T18:24:00.325+08:00",
       "error": null
     },
     "claude": {
@@ -32,15 +32,15 @@ window.DASH_DATA = {
         {
           "name": "5小时",
           "usedPct": 16,
-          "resetAt": "2026-07-27T22:49:59.274+08:00"
+          "resetAt": "2026-07-27T22:50:00.275+08:00"
         },
         {
           "name": "7天",
           "usedPct": 39,
-          "resetAt": "2026-07-30T19:59:59.274+08:00"
+          "resetAt": "2026-07-30T20:00:00.275+08:00"
         }
       ],
-      "fetchedAt": "2026-07-27T18:15:00.353+08:00",
+      "fetchedAt": "2026-07-27T18:24:00.373+08:00",
       "error": null
     },
     "codex": {
@@ -49,11 +49,11 @@ window.DASH_DATA = {
       "windows": [
         {
           "name": "周",
-          "usedPct": 39,
+          "usedPct": 40,
           "resetAt": "2026-08-02T03:30:23.000+08:00"
         }
       ],
-      "fetchedAt": "2026-07-27T18:15:00.354+08:00",
+      "fetchedAt": "2026-07-27T18:24:00.375+08:00",
       "error": null
     },
     "kimi": {
@@ -77,7 +77,7 @@ window.DASH_DATA = {
           "observedAt": "2026-07-26T08:00:03.880+08:00"
         }
       ],
-      "fetchedAt": "2026-07-27T18:15:00.363+08:00",
+      "fetchedAt": "2026-07-27T18:24:00.385+08:00",
       "error": null
     }
   },
